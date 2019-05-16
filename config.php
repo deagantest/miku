9d6600c04e9f1bbf3eab7f3e530a2b64d4d596d1
deagantest <48824737+deagantest@users.noreply.github.com>
2019-05-15 23:40:24 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'localhost';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'minekoto';
+$config['db']['password'] = '3children';
+$config['db']['dbname'] = 'minekoto';
+
+$config['fullUnicode'] = true;
